1b0faa302aa49cd72220e37134253701f515d52f
Malkit Singh <malkit@xenondigilabs.com>
2018-04-25 08:14:37 +0530

    added exception in nonetype results

--- callback_plugins/json_cb.py
+++ callback_plugins/json_cb.py
@@ -16,15 +16,18 @@ class CallbackModule(CallbackBase):
     	countes = 0
     	self.custom_result = dict(name=self.tasks[result._task._uuid],result=result._result, host=result._host.get_name())
         ranges = self.custom_result["result"].get("results")
-        for count in ranges:
-        	if count["failed"] != False:
-        		countes += 1
-        self.custom_result["failures"] = countes
-        if self.custom_result['failures'] > 0:
-    		self.custom_result["failures"] = True
-    	else:
-    		self.custom_result["failures"] = False
-        print (json.dumps(self.custom_result))
+        if not ranges:
+        	print(json.dumps(self.custom_result))
+        else:
+	        for count in ranges:
+	        	if count["failed"] != False:
+	        		countes += 1
+	        self.custom_result["failures"] = countes
+	        if self.custom_result['failures'] > 0:
+	    		self.custom_result["failures"] = True
+	    	else:
+	    		self.custom_result["failures"] = False
+	        print (json.dumps(self.custom_result))
 
     def v2_playbook_on_task_start(self, task, is_conditional):
         self.tasks[task._uuid] = task.name
